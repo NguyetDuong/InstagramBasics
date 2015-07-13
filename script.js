8900bce9eaa8321f8c96d8f19f2340db2a672149
Nguyet Duong <nguyetduong.minh@gmail.com>
2015-07-13 09:08:50 -0700

    initial adding files.

---
+++ script.js
@@ -0,0 +1,23 @@
+var CURRENT_YEAR = 2015;
+var mimi_circa_09 = [CURRENT_YEAR + " .one of the best years of my life.", "Should I use BlogSpot or WordPress?", 
+						"Male honeybees only get to have sex once in their life...because it kills them. During ejaculation the penis breaks off inside the female, and the male dies within hours.",
+						"I LOVE YOU MILEY CYRUSS. :D", "My hair looks better when I straighten it D= right now it looks like a dried cat fur",
+						"I fucking hate it when random people add me -____-' asses and now posting shit all over. son of a bitch",
+						"I hate it when fucking hate it when people spam me with random shit I don't even give a shit about. --Still hate a bitch(:",
+						"I LOVE YOU INSTAGRAM. WILL YOU MARRY ME? cus i tell you every shit.",
+						"I shall start my hw tomorrow for sure o_o'", "I shall not cut my own hair ever again.", 
+						"People are hating me because I am married to MILEY CYRUS</3", "Y'all. I just want you to know that I'm officially married to MILEYY CYRUS♥ VIA AIM LMFAO.",
+						"I swear to god. Mother bitch. One of these days I'm going to lose it and you're going have it and you are going to be fucking sorry.",
+						"I burnt my whole right hand because my hair curler was on and it fell. Me being me tried to catch and save my $100 straighter//curler. I ended up burning my right hand. FML.",
+						"I have too much social life now. LOL", "I hate it when fucking people I don't know add me -___-'' <~ pissed face",
+						"Seriously I'm so fucking annoyed that everyone likes my shit when I'm never even gonna do that shit to say that your top stalker.",
+						"I had to format my whole computer because I had virus D:", "HOW DARE PPL SAY EDWARD CULLEN IS GAY D:. I heart emoticon EDWARD CULLEN</3",
+						"Who thinks I'm a wh0re, like this post<33", "EDWARD CULLEN - my imgination Edward mind you- IS FUCKING ASS SHITTINGLY HOT.heart emoticon I'd fuck him any day.",
+						"It's over. ITS OVER D: < DONT EVER TALK TO ME AGAIN D: <", "IN UC. I'm sooo taking the class modern interpretations of sexuality and pornography.",
+						"MAN That guy was hot o_O LOL", 
+						"Q: What goes in hard and pink, but comes out soft and mushy? A: Bubblegum -- and you should be ashamed of yourself. I'd be lying if I didn't think it was that LOL.",
+						"Who has tequila? I have margarita LOL. LOOKING FOR SOMEONE WITH TAQUILA D;", 
+						"PEOPLE. Does HARD and DARK rhymes? D:< IT DOES. RIGHT?!", "Things happens when I gasp because the dude did add me O: < GASP GASP GASP. UN-BE-LIEV-A-FUCKING-BLE",
+						"Boys come to me for sex :3", "OH MY FUCKING GOD. SOMEONE GET ME CPR. IM FAINTING. OMFG OMFG OMFG. I CANT FUCKING BELIEVE IT!",
+						"Cus i fucking felt like it ;x"];
+
